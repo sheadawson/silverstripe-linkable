62a3198196b8b65f874cfefc1196caf14b74162c
jcarter <josh@symbiote.com.au>
2019-10-01 17:38:38 +1000

    Fix link component to output expected HTML structure

--- client/src/components/linkfield.js
+++ client/src/components/linkfield.js
@@ -1,63 +1,40 @@
-import React, { PureComponent } from 'react';
+import * as React from 'react';
 import PropTypes from 'prop-types';
 
-class LinkField extends PureComponent {
+class LinkField extends React.PureComponent {
 
     constructor(props) {
         super(props);
     }
 
-    renderExisting() {
-        const {
-            id,
-            name,
-            value,
-            label,
-            url,
-        } = this.props;
-
-        return (
-            <div>
-                <a className="link" href={url} target="_blank" style={{ paddingRight: '10px' }}>{label}</a>
-                <button href="#" className="linkfield-button btn btn-primary" onClick={this.handleOpenModal}>Edit</button>
-                <button href="#" className="linkfield-remove-button btn btn-danger">Remove</button>
-                <input id={id} name={name} value={value} type="text" className="link" style={{ display: 'none' }} />
-                <div className='linkfield-dialog'></div>
-            </div>
-        );
-    }
-
-    renderNone() {
-        const {
-            id,
-            name,
-            value,
-        } = this.props;
-
-        return (
-            <div>
-                <button href="#" className="linkfield-button btn btn-primary font-icon-plus-circled" onClick={this.handleOpenModal}>Add Link</button>
-                <input id={id} name={name} value={value} type="text" className="link" style={{ display: 'none' }} />
-                <div className='linkfield-dialog'></div>
-            </div>
-        );
-    }
-
     render() {
         const {
             id,
-            value,
+            name,
             title,
+            label,
+            value,
+            url,
             holderId,
         } = this.props;
 
-        let have_val = (value != undefined && value != '' && value != '0');
+        let have_link = (value != undefined && value != '' && value != '0');
+
+        let link = (<a className="link" href={url} style={{ paddingRight: '10px' }}>{label}</a>);
+        let add = (<button href="#" className="linkfield-button btn btn-primary font-icon-plus-circled">Add Link</button>);
+        let edit = (<button href="#" className="linkfield-button btn btn-primary">Edit</button>);
+        let remove = (<button href="#" className="linkfield-remove-button btn btn-danger">Remove</button>);
 
         return (
-            <div className="field form-group">
-                <label className="form__field-label" for={id}>{title}</label>
-                <div id={holderId} className="form__field-holder">
-                    {have_val ? this.renderExisting() : this.renderNone()}
+            <div className="field form-group link" id={holderId}>
+                <label className="form__field-label" id={`title-${id}`} htmlFor={id}>{title}</label>
+                <div className="form__field-holder">
+                    <div className='linkfield-dialog'></div>
+                    <input type="hidden" className="link" id={id} name={name} value={value} />
+                    {have_link && link}
+                    {have_link && edit}
+                    {have_link && remove}
+                    {!have_link && add}
                 </div>
             </div>
         );
